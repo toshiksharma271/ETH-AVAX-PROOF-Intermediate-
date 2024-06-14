64d00b2f39b0466e9b6a45100f8550fc82be44ca
toshiksharma271 <98310467+toshiksharma271@users.noreply.github.com>
2024-06-14 17:59:09 +0530

    Challenge-1-Completed

--- AdminAccess.sol
+++ AdminAccess.sol
@@ -1,1 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
 
+contract AdminToken{
+    string public TokenName = "Baingan";
+    string public Abbrev = "BG";
+    uint public totalSupply = 0;
+    address owner;
+
+constructor(){
+    owner=msg.sender;
+}
+
+mapping(address => uint )public balances;
+
+modifier ownerAccess() {
+    require(owner==msg.sender,"Admin permission required!");
+    _;
+}
+
+
+function mint(address _address,uint _value)public ownerAccess{
+    totalSupply+=_value;
+    balances[_address]+=_value;
+}
+
+function burn(address _address,uint _value)public ownerAccess{
+    totalSupply-=_value;
+    balances[_address]-=_value;
+}
+
+}
